2f79e51006f328e5232a11d5127bad2f85e9b9ad
Jashan <jashandeep4329@gmail.com>
2025-04-22 15:17:01 +0530

    added some of my learnings to the readme file

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -13,13 +13,22 @@ const uploadOnCloudinary = async(localFilePath) => {
     try{
         if(!localFilePath) return null
         //upload file on category
-        cloudinary.uplaoder.upload(localFilePath, {
+       const response = await cloudinary.uplaoder.upload(localFilePath, {
                 resource_type: "auto"
         })
 
         //file has been uploaded successfully
-        console.log("File has been uploaded successfully");
+        console.log("File has been uploaded successfully", 
+           response.url
+        );
+        return response;
+
+    
     }catch(error){
+        fs.unlinkSync(localFilePath) //Remove the locally saved temporary file if the request is failed
 
     }
 }
+
+
+export {uploadOnCloudinary}
